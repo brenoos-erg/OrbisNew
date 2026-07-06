502f620e43f290c3abf4e8234ddd971bdc2084eb
Breno Sousa <brenoos@ergengenharia.com.br>
2026-07-06 08:23:24 -0300

    atualiza sistema

--- rh-dp-solicitacoes/src/lib/documents/documentFamilyRules.ts
+++ rh-dp-solicitacoes/src/lib/documents/documentFamilyRules.ts
@@ -1,5 +1,5 @@
-const NON_CONTROLLED_PREFIXES = new Set(['DOCEXT', 'RQ', 'LEG', 'DD', 'MAN'])
-const CONTROLLED_PREFIXES = new Set(['PG', 'IT', 'COD', 'POL', 'MSIG'])
+const NON_CONTROLLED_PREFIXES = new Set(['POL', 'DOCEXT', 'RQ', 'LEG', 'MAN'])
+const CONTROLLED_PREFIXES = new Set(['PG', 'IT', 'DD', 'MSIG'])
 
 export type DocumentPipelineFamily = 'non-controlled-native' | 'controlled-pdf'
 
